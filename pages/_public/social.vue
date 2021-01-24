90b9eb4ec7f6b2f0ce28daad5c145dc2d7bf83e6
Blake Kostner <git@btkostner.io>
2021-01-24 01:38:45 -0700

    update twitter social media image

--- pages/_public/social.vue
+++ pages/_public/social.vue
@@ -4,29 +4,28 @@
  */
 
 <template>
-  <div class="min-h-screen grid place-items-center place-content-center">
+  <div class="min-h-screen grid place-items-center place-content-center bg-gray-900">
     <div
-      :class="`ring-4 ring-offset-2 ring-black rounded flex items-center ${field}`"
+      :class="`ring-4 ring-offset-2 ring-black rounded flex items-center relative overflow-hidden ${field}`"
       id="image"
     >
       <div class="m-8">
-        <h1 class="text-6xl leading-9 font-extrabold font-display text-gray-900">
+        <h1 class="text-6xl leading-9 font-extrabold font-display text-red-600">
           {{ post.title }}
         </h1>
 
-        <h2 class="mt-6 text-3xl leading-7 text-gray-500">
+        <h2 class="mt-6 text-3xl leading-7 text-gray-400">
           by {{ post.authorName }}
         </h2>
 
-        <div class="text-base leading-6 font-medium mt-6">
-          <nuxt-link
-            class="text-red-700 hover:text-red-800"
-            :aria-label="`Read &quot;${post.title}&quot;`"
-            :to="`/blog/${post.slug}`"
-          >
-            Read more →
-          </nuxt-link>
+        <div class="text-base leading-6 font-medium mt-8 text-red-500">
+          Read more →
         </div>
+      </div>
+
+      <div class="absolute bottom-0 right-0 h-96 w-96 -m-48 transform -rotate-45">
+        <div class="h-8 w-full mt-16 bg-gray-800" />
+        <div class="h-8 w-full mt-6 bg-gray-800" />
       </div>
     </div>
   </div>
