5641124258b86909fbed5899c9dcbc7fcfdd648f
btkostner <btkostner@users.noreply.github.com>
2021-08-18 04:35:44 +0000

    deploy: 206bcec80c38740780038f0c089b7cc22d4c44e5

--- public/static/1626647477/blog/2020-01-09-hello-world/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002Fpublic\u002Fstatic\u002F1626647477",layout:"default",error:a,serverRendered:b,routePath:"\u002Fblog\u002F2020-01-09-hello-world",config:{_app:{basePath:"\u002F",assetsPath:"\u002Fpublic\u002F",cdnURL:a},content:{dbHash:"8d1febae"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
