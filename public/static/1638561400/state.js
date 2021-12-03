249808859c56b0352342e4e145bab90dd9e03360
btkostner <btkostner@users.noreply.github.com>
2021-12-03 19:56:44 +0000

    deploy: 40c5b935d0e850b95336a473332b7bf9ab2493e4

---
+++ public/static/1638561400/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c,d){return {staticAssetsBase:"\u002Fpublic\u002Fstatic\u002F1638561400",layout:"default",error:a,serverRendered:b,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002Fpublic\u002F",cdnURL:a},content:{dbHash:"655705fd"}},colorMode:{preference:d,value:d,unknown:b,forced:false}}}(null,true,"\u002F","system"));
