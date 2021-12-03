6bdb7e33c9cfc54c9781fee9e783d3cdf99c2099
Blake Kostner <git@btkostner.io>
2021-12-03 12:58:34 -0700

    Change up the homepage

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,11 @@
   <main class="flex-1 flex flex-col w-full">
     <div class="my-8 mx-8 md:my-16 md:mx-16 lg:my-24 lg:mx-24 xl:my-44 xl:mx-44">
       <h1 class="text-gray-800 dark:text-gray-200 mt-4 text-3xl md:mt-8 md:text-4xl lg:text-6xl xl:text-8xl">
-        Blake Kostner
+        Poor Mans PR preview URL
       </h1>
 
       <h2 class="text-red-700 dark:text-red-600 uppercase mt-1 text-lg md:text-xl lg:mt-2 lg:text-2xl xl:text-4xl">
-        Full stack web developer
+        Mwhahahaha it works!!!
       </h2>
     </div>
   </main>
