b0ade409be403924a57461246d78a88b37d9f3f3
btkostner <btkostner@users.noreply.github.com>
2021-12-15 22:12:01 +0000

    deploy: 9ebb9e007d8fccaa12ccdd57805ee6c8f7ec8ca5

--- pr/12/public/static/1639606203/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fblog","\u002Ftil","\u002Fblog\u002F2020-01-09-hello-world","\u002Fblog\u002F2020-01-24-better-nuxt-content-code-highlighting","\u002Ftil\u002Felixir-github-action"]})
