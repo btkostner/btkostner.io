99d48219855c7a93b0a2291aca23a929101665cd
Blake Kostner <git@btkostner.io>
2022-12-14 19:30:26 -0700

    setup mastodon webfinger redirect

---
+++ server/routes/.well-known/webfinger.ts
@@ -0,0 +1,18 @@
+import { createError, getQuery, sendRedirect } from "h3";
+
+export default defineEventHandler(async (event) => {
+  const query = getQuery(event);
+
+  if (
+    typeof query.resource === "string" &&
+    query.resource.startsWith("acct:btkostner")
+  ) {
+    return sendRedirect(
+      event,
+      "https://mastodon.social/.well-known/webfinger?resource=acct:btkostner@mastodon.social",
+      307
+    );
+  } else {
+    return createError({ status: 404 });
+  }
+});
