cd95a8089d00243147586a5a78638ed94e427b1a
Blake Kostner <git@btkostner.io>
2025-08-01 17:29:11 -0700

    fix linting

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,3 +1,7 @@
 import withNuxt from "./.nuxt/eslint.config.mjs";
 
-export default withNuxt();
+export default withNuxt().override("nuxt/vue/rules", {
+  rules: {
+    "vue/html-self-closing": "off",
+  },
+});
